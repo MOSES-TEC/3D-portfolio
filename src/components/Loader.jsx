60d60a16b6521144691da03891d9d1c4eb1365ed
MOSES Y <ymoses546@gmail.com>
2025-05-11 12:06:10 +0530

    implementation of 3d model

---
+++ src/components/Loader.jsx
@@ -0,0 +1,48 @@
+import { Html, useProgress } from "@react-three/drei";
+
+const CanvasLoader = () => {
+  const { progress } = useProgress();
+  return (
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className='canvas-loader'></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
+
+export default CanvasLoader;
+
+// ✅ CanvasLoader.jsx
+// import { Html } from '@react-three/drei';
+
+// const CanvasLoader = () => {
+//   return (
+//     <Html center>
+//       <span className="loader">Loading...</span>
+//     </Html>
+//   );
+// };
+
+// export default CanvasLoader;
+
+
+
+
